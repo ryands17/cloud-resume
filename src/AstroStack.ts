754bbd377d3be9507bd5a81f16f8b8216d6cd994
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2023-10-26 15:36:32 +0200

    add cloudfront function

--- src/AstroStack.ts
+++ src/AstroStack.ts
@@ -1,10 +1,31 @@
-import { StaticSite, Function } from 'sst/constructs';
-import { FunctionEventType } from 'aws-cdk-lib/aws-cloudfront';
+import { StaticSite } from 'sst/constructs';
+import {
+  FunctionEventType,
+  Function,
+  FunctionCode,
+} from 'aws-cdk-lib/aws-cloudfront';
 import { Stack } from 'sst/constructs';
+
+const code = `function handler(event) {
+  let request = event.request;
+  let uri = request.uri;
+
+  // Check whether the URI is missing a file name.
+  if (uri.endsWith('/')) {
+    request.uri += 'index.html';
+  }
+  // Check whether the URI is missing a file extension.
+  else if (!uri.includes('.')) {
+    request.uri += '/index.html';
+  }
+
+  return request;
+}`;
 
 export function AstroStack({ stack }: { stack: Stack }) {
   const fixSubPages = new Function(stack, 'fixSubPages', {
-    handler: 'src/sst/fixSubPages.handler',
+    code: FunctionCode.fromInline(code),
+    comment: 'To redirect to the correct page in Astro',
     functionName: 'fixSubPages',
   });
 
