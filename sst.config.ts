96c473e4d79a80a3f78ef2aa53102003cca2e0f6
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2023-10-26 17:15:14 +0200

    add region

--- sst.config.ts
+++ sst.config.ts
@@ -15,7 +15,7 @@ class AutoPublishCloudFrontFunctions implements IAspect {
 
 export default {
   config(_input) {
-    return { name: 'cloud-resume' };
+    return { name: 'cloud-resume', region: 'eu-west-1' };
   },
   stacks(app) {
     app.stack(AstroStack);
