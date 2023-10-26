5ae1d2361ca86bbd178a97620f3caa853bfc84b8
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2023-10-26 14:53:00 +0200

    fix astro config to create a static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
-import { defineConfig } from "astro/config";
-import aws from "astro-sst/lambda";
+// import aws from 'astro-sst/lambda';
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
+import { SITE_METADATA } from './src/consts.ts';
+
+// https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: aws(),
+  site: SITE_METADATA.siteUrl,
+  integrations: [mdx(), sitemap(), tailwind()],
 });
--- src/AstroStack.ts
+++ src/AstroStack.ts
@@ -1,8 +1,9 @@
-import { AstroSite } from 'sst/constructs';
+import { AstroSite, StaticSite } from 'sst/constructs';
 import { Stack } from 'sst/constructs';
 
 export function AstroStack({ stack }: { stack: Stack }) {
-  const site = new AstroSite(stack, 'site');
+  const site = new StaticSite(stack, 'site', { path: 'dist' });
+
   stack.addOutputs({
     url: site.url,
   });
