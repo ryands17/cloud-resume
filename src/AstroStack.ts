1077f6e8928044d3599d42113c702d3b071896c1
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2023-10-26 22:09:07 +0200

    add custom resource to fetch domain and add to cloudfront

--- src/AstroStack.ts
+++ src/AstroStack.ts
@@ -1,10 +1,13 @@
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as cr from 'aws-cdk-lib/custom-resources';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { StaticSite } from 'sst/constructs';
 import { Stack } from 'sst/constructs';
 
 const acmArnPath = '/portfolio/acmCertArn';
+const domain = 'ryan17.dev';
 
 const code = `function handler(event) {
   var request = event.request;
@@ -29,8 +32,34 @@ export function AstroStack({ stack }: { stack: Stack }) {
     functionName: 'fixSubPages',
   });
 
+  const fetchCertArn = new cr.AwsCustomResource(stack, 'fetchCertArn', {
+    resourceType: 'Custom::FetchCertArn',
+    onUpdate: {
+      service: 'SSM',
+      action: 'getParameter',
+      region: 'us-east-1',
+      parameters: { Name: acmArnPath },
+      physicalResourceId: cr.PhysicalResourceId.of(`cert${acmArnPath}`),
+    },
+    policy: cr.AwsCustomResourcePolicy.fromSdkCalls({
+      resources: [`arn:aws:ssm:us-east-1:*:parameter${acmArnPath}`],
+    }),
+    logRetention: RetentionDays.ONE_DAY,
+  });
+
   const site = new StaticSite(stack, 'site', {
     path: 'dist',
+    customDomain: {
+      domainName: domain,
+      alternateNames: [`www.${domain}`],
+      cdk: {
+        certificate: acm.Certificate.fromCertificateArn(
+          stack,
+          'cert',
+          fetchCertArn.getResponseField('Parameter.Value'),
+        ),
+      },
+    },
     cdk: {
       distribution: {
         defaultBehavior: {
@@ -51,7 +80,6 @@ export function AstroStack({ stack }: { stack: Stack }) {
 }
 
 export function GlobalStack({ stack }: { stack: Stack }) {
-  const domain = 'ryan17.dev';
   // ACM certificate for the domain
   const cert = new acm.Certificate(stack, 'personalDomain', {
     domainName: domain,
@@ -64,8 +92,12 @@ export function GlobalStack({ stack }: { stack: Stack }) {
     threshold: 45,
   });
 
-  new StringParameter(stack, 'certArn', {
+  const certArn = new StringParameter(stack, 'certArn', {
     parameterName: acmArnPath,
     stringValue: cert.certificateArn,
+  });
+
+  stack.addOutputs({
+    certArn: certArn.stringValue,
   });
 }
