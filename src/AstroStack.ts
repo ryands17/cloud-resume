0e2d8a6ea81e7a102bbaf1a2b91c4bc26b16b694
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2023-10-26 15:13:47 +0200

    check if cloudfront function redirect works

--- src/AstroStack.ts
+++ src/AstroStack.ts
@@ -1,8 +1,27 @@
-import { AstroSite, StaticSite } from 'sst/constructs';
+import { StaticSite, Function } from 'sst/constructs';
+import { FunctionEventType } from 'aws-cdk-lib/aws-cloudfront';
 import { Stack } from 'sst/constructs';
 
 export function AstroStack({ stack }: { stack: Stack }) {
-  const site = new StaticSite(stack, 'site', { path: 'dist' });
+  const fixSubPages = new Function(stack, 'fixSubPages', {
+    handler: 'sst/fixSubPages.handler',
+  });
+
+  const site = new StaticSite(stack, 'site', {
+    path: 'dist',
+    cdk: {
+      distribution: {
+        defaultBehavior: {
+          functionAssociations: [
+            {
+              function: fixSubPages,
+              eventType: FunctionEventType.VIEWER_REQUEST,
+            },
+          ],
+        },
+      },
+    },
+  });
 
   stack.addOutputs({
     url: site.url,
