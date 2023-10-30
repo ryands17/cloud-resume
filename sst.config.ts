a84a58ecaef135276859800c2006f77be5ae2d27
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2023-10-30 15:44:41 +0100

    fix astro not working locally with SST in the src folder

--- sst.config.ts
+++ sst.config.ts
@@ -1,8 +1,8 @@
-import { AstroStack, GlobalStack } from '@/AstroStack';
+import packageJson from './package.json';
 import { Tags, Aspects, type IAspect } from 'aws-cdk-lib/core';
 import { type IConstruct } from 'constructs';
-import packageJson from './package.json';
-import type { SSTConfig } from 'sst';
+import { AstroStack, GlobalStack } from './sst/AstroStack';
+import { type SSTConfig } from 'sst';
 import { CfnFunction } from 'aws-cdk-lib/aws-cloudfront';
 
 class AutoPublishCloudFrontFunctions implements IAspect {
